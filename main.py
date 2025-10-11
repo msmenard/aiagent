ebd0d9e597a26ac6cd7b8010de6554b0800b64ca
msmenard <matthew.s.menard@gmail.com>
2025-10-11 10:17:20 -0400

    Added  flag to see user prompt, prompt tokens, and response tokens.

--- main.py
+++ main.py
@@ -13,11 +13,15 @@ client = genai.Client(api_key=api_key)
 
 
 def main():
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         print("Please provide a prompt for the AI Agent to process.\nuv run main.py <prompt>")
         sys.exit(1)
 
     user_prompt = sys.argv[1]
+    if len(sys.argv) == 3:
+        verbose_flag = sys.argv[2]
+    else:
+        verbose_flag = None
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
@@ -27,10 +31,14 @@ def main():
         model='gemini-2.0-flash-001',
         contents=messages,
     )
-
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    if verbose_flag == "--verbose":
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
 
 
 if __name__ == "__main__":
