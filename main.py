5967c6c801d2b21907fe5bfd353e31b95db4f02c
msmenard <matthew.s.menard@gmail.com>
2025-10-14 20:41:36 -0400

    Added first step in the ability of Agent to suggest functions and args to accomplish tasks.

--- main.py
+++ main.py
@@ -3,15 +3,29 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
 
-system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
 
-
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
 
 def main():
     if len(sys.argv) < 2:
@@ -31,16 +45,27 @@ def main():
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt)
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt),
+        
     )
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
-    print(response.text)
+
+    function_call_part = response.function_calls or []
+
+    if function_call_part:
+        for call in function_call_part:
+            print(f"Calling function: {function_call_part[0].name}({function_call_part[0].args})")
+    else:
+        print(response.text)
 
     if verbose_flag == "--verbose":
         print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {prompt_tokens}")
         print(f"Response tokens: {response_tokens}")
+
+    
 
 
 if __name__ == "__main__":
